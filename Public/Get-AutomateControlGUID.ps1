5c0dd527bef32104e13db81bf44a0b66c835114e
Darren White <dwhite@apex.com>
2019-02-13 13:03:37 -0800

    Eliminating redundant code from Get-AutomateControlGUID

--- Public/Get-AutomateControlGUID.ps1
+++ Public/Get-AutomateControlGUID.ps1
@@ -1,31 +1,18 @@
 function Get-AutomateControlGUID {
-    param
-    (
-        [Parameter(Mandatory = $true, Position = 0,ValueFromPipeline = $true, ValueFromPipelineByPropertyName=$true)]
-        [Alias('Id')]
-        [int16[]]$ComputerID
-    )
+  <#
+  This function should not be needed.
+  Get-CWAControl replaces it.
+  #>
+  param
+  (
+      [Parameter(Mandatory = $true, Position = 0,ValueFromPipeline = $true, ValueFromPipelineByPropertyName=$true)]
+      [Alias('Id')]
+      [int16[]]$ComputerID
+  )
 
-    
   process {
     foreach ($ComputerIDSingle in $ComputerID) {
-        $url = ($Script:CWAUri + "/v1/extensionactions/control/$ComputerIDSingle")
-
-        $OurResult = [pscustomobject]@{
-          ComputerId = $ComputerIdSingle
-        }
-
-        $Result = Invoke-RestMethod -Uri $url -Headers $script:CWACredentials -ContentType "application/json"
-        if (-not ([string]::IsNullOrEmpty($Result))) {
-            $Position = $Result.IndexOf("=");
-            $ControlGUID = ($Result.Substring($position + 1)).Substring(0, 36)
-            
-            $OurResult | Add-Member -NotePropertyName ControlGuid -NotePropertyValue $ControlGuid -PassThru | Write-Output
-        }
-        else {
-            $OurResult | Add-Member -NotePropertyName ControlGuid -NotePropertyValue "No GUID Found" -PassThru | Write-Output
-        }
+      Get-CWAControl -ComputerID $ComputerIDSingle
     }
   }
-
 }
