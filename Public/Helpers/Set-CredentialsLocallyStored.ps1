a6f3eeb154a2f69245afa3f371534bd0a1f70ef9
Darren White <github@darrenwhite.org>
2019-02-19 19:19:42 -0800

    Credential Storage Fixes

--- Public/Helpers/Set-CredentialsLocallyStored.ps1
+++ Public/Helpers/Set-CredentialsLocallyStored.ps1
@@ -50,18 +50,17 @@ function Set-CredentialsLocallyStored {
         New-Item -ItemType Directory -Force -Path $CredentialDirectory | ForEach-Object{$_.Attributes = "hidden"}
     }
 
-
     If ($Automate) {
+        If (!$Save) {
+            Connect-AutomateAPI -Server '' -Force
+        }
+
         $StoreVariables = @(
             @{'Name' = 'CWAServer'; 'Scope' = 'Script'},
             @{'Name' = 'CWACredentials'; 'Scope' = 'Script'},
             @{'Name' = 'CWATokenKey'; 'Scope' = 'Script'},
             @{'Name' = 'CWATokenInfo'; 'Scope' = 'Script'}
         )
-
-        If (!$Save) {
-            Connect-AutomateAPI -Server '' -Force
-        }
 
         $StoreBlock = [pscustomobject]@{}
         $CredentialPath = "$($CredentialDirectory)\Automate - Credentials.txt"
@@ -86,35 +85,21 @@ function Set-CredentialsLocallyStored {
                 $Null = $StoreBlock | Add-Member -NotePropertyName $($SaveVar.Name) -NotePropertyValue (Get-Variable @SaveVar -ValueOnly)
             }
         }
-<#
-        } Else {
-            $TempAutomateServer = $Null
-            Do {
-                $TempAutomateServer = Read-Host -Prompt "Please enter your Automate Server address, IE: rancor.hostedrmm.com"
-                $TempAutomateServer = $TempAutomateServer -replace '^https?://','' -replace '/.*','' 
-                If (!($TempAutomateServer -match '^[a-z0-9][a-z0-9\.\-]*$')) {Write-Host "Server address is missing or in invalid format."}
-            } Until ($TempAutomateServer -match '^[a-z0-9][a-z0-9\.\-]*$')
-            $TempAutomateUsername = Read-Host -Prompt "Please enter your Automate Username"
-            $TempAutomatePassword = Read-Host -Prompt "Please enter your Automate Password" -AsSecureString
-            $Null = $StoreBlock | Add-Member -NotePropertyName 'CWAServer' -NotePropertyValue $TempAutomateServer
-            $Null = $StoreBlock | Add-Member -NotePropertyName 'CWACredentials' -NotePropertyValue @{'UserName'=$TempAutomateUsername; 'Password'=($TempAutomatePassword | ConvertFrom-SecureString)}
-        }
-#>
 
         $StoreBlock | ConvertTo-JSON -Depth 10 | Out-File -FilePath $CredentialPath -Force -NoNewline
         Write-Output "Automate Credentials Set"
     }
 
     If ($Control) {
+        If (!$Save) {
+            Connect-ControlAPI -Server '' -Force
+        }
+
         $StoreVariables = @(
             @{'Name' = 'ControlAPICredentials'; 'Scope' = 'Script'},
             @{'Name' = 'ControlServer'; 'Scope' = 'Script'},
             @{'Name' = 'ControlAPIKey'; 'Scope' = 'Script'}
         )
-
-        If (!$Save) {
-            Connect-ControlAPI -Server '' -Force
-        }
 
         $StoreBlock = [pscustomobject]@{}
         $CredentialPath = "$($CredentialDirectory)\Control - Credentials.txt"
@@ -139,14 +124,6 @@ function Set-CredentialsLocallyStored {
                 $Null = $StoreBlock | Add-Member -NotePropertyName $($SaveVar.Name) -NotePropertyValue (Get-Variable @SaveVar -ValueOnly)
             }
         }
-<#        } Else {
-            $TempControlServer = Read-Host -Prompt "Please enter your Control Server address, the full URL. IE https://control.rancorthebeast.com:8040" 
-            $TempControlUsername = Read-Host -Prompt "Please enter your Control Username"
-            $TempControlPassword = Read-Host -Prompt "Please enter your Control Password" -AsSecureString
-            $Null = $StoreBlock | Add-Member -NotePropertyName 'ControlServer' -NotePropertyValue $TempControlServer
-            $Null = $StoreBlock | Add-Member -NotePropertyName 'ControlAPICredentials' -NotePropertyValue @{'UserName'=$TempControlUsername; 'Password'=($TempControlPassword | ConvertFrom-SecureString)}
-#        }
-#>        
 
         $StoreBlock | ConvertTo-JSON -Depth 10 | Out-File -FilePath $CredentialPath -Force -NoNewline
         Write-Output "Control Credentials Set"
@@ -156,7 +133,7 @@ function Set-CredentialsLocallyStored {
         $StoreBlock = [pscustomobject]@{}
         $CredentialPath = "$($CredentialDirectory)\$($CredentialDisplayName).txt"
         $CustomCredentials = Get-Credential -Message "Please enter the Custom Username and Password to store"
-        $Null = $StoreBlock | Add-Member -NotePropertyName 'CustomCredentials' -NotePropertyValue @{'UserName'=$CustomCredentials.Password; 'Password'=($CustomCredentials.Password | ConvertFrom-SecureString)}
+        $Null = $StoreBlock | Add-Member -NotePropertyName 'CustomCredentials' -NotePropertyValue @{'UserName'=$CustomCredentials.UserName; 'Password'=($CustomCredentials.Password | ConvertFrom-SecureString)}
 
         $StoreBlock | ConvertTo-JSON -Depth 10 | Out-File -FilePath $CredentialPath -Force -NoNewline
         Write-Output "Custom Credentials Set for $($CredentialDisplayName)"
