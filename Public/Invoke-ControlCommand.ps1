f553775d9142c2a80ab30aa79c484b1885b6e9a9
Darren White <github@darrenwhite.org>
2019-02-23 10:18:51 -0800

    Adjusted wait between status checks.

--- Public/Invoke-ControlCommand.ps1
+++ Public/Invoke-ControlCommand.ps1
@@ -63,9 +63,10 @@ function Invoke-ControlCommand {
         function Get-SleepDelay {
             param($seconds = 1, $totalseconds)
             if (!$totalseconds) {$totalseconds = $seconds * 2}
-            Try {$Delay = [math]::Truncate([math]::pow(($totalseconds / 2) - [math]::Abs($seconds - ($totalseconds / 2)), 1 / 3))}
+            Try {$Delay = [math]::Ceiling([math]::pow(($totalseconds / 2) - [math]::Abs($seconds - ($totalseconds / 2)), 1 / 3))}
             Catch {$Delay = 1}
-            Finally {If ([double]::IsNaN($Delay)) {$Delay = 1}}
+            Finally {If ([double]::IsNaN($Delay) -or $Delay -lt 1) {$Delay = 1}}
+            Write-Debug "Sleep Delay is $Delay"
             return $Delay
         }
 
