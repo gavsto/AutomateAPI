45f42cb1dfa580f6ecba583008fba2fd3e00665a
Darren White <dwhite@apex.com>
2020-08-10 03:52:26 -0700

    Reformatted commands for better readibility

--- Public/Repair-AutomateAgent.ps1
+++ Public/Repair-AutomateAgent.ps1
@@ -65,7 +65,15 @@ function Repair-AutomateAgent {
                      Write-Host -BackgroundColor DarkGray -ForegroundColor Yellow "$($_.ComputerID) - $($_.ComputerName) - Attempting to $Action Automate Services - job will be submitted to online systems"
                      $_
                   }
-               } | Invoke-ControlCommand -Powershell -Command "(new-object Net.WebClient).DownloadString('$($LTPoShURI)') | iex; Try {Get-LTServiceInfo} Catch {'Error getting service settings. Checking LTErrors.txt'; Get-Content `"`${env:windir}\ltsvc\lterrors.txt`" | Select-Object -Last 100 }" -TimeOut 60000 -MaxLength 10240 -BatchSize $BatchSize -OfflineAction Skip -ResultPropertyName $RepairProperty -PassthroughObjects
+               } | Invoke-ControlCommand -Powershell -Command @"
+Try { 
+  (new-object Net.WebClient).DownloadString('$($LTPoShURI)') | iex
+  Get-LTServiceInfo
+} Catch {
+  'Error getting service settings. Checking LTErrors.txt'
+  Get-Content "`${env:windir}\ltsvc\lterrors.txt" | Select-Object -Last 100
+}
+"@ -TimeOut 60000 -MaxLength 10240 -BatchSize $BatchSize -OfflineAction Skip -ResultPropertyName $RepairProperty -PassthroughObjects
                $ObjectCapture | Where-Object {$_.OperatingSystemName -like '*OS X*'}  | ForEach-Object {
                   If ($PSCmdlet.ShouldProcess("Automate Services on $($_.ComputerID) - $($_.ComputerName)",$Action)) {
                      Write-Host -BackgroundColor DarkGray -ForegroundColor Yellow "$($_.ComputerID) - $($_.ComputerName) - Attempting to $Action Automate Services - job will be submitted to online systems"
@@ -103,19 +111,30 @@ echo '}'
                      Write-Host -BackgroundColor DarkGray -ForegroundColor Yellow "$($_.ComputerID) - $($_.ComputerName) - Attempting to $Action Automate Services - job will be submitted to online systems"
                      $_
                   }
-               } | Invoke-ControlCommand -Powershell -Command "(new-object Net.WebClient).DownloadString('$($LTPoShURI)') | iex; Update-LTService" -TimeOut 120000 -MaxLength 10240 -BatchSize $BatchSize -OfflineAction Skip -ResultPropertyName $RepairProperty -PassthroughObjects
+               } | Invoke-ControlCommand -Powershell -Command @"
+(new-object Net.WebClient).DownloadString('$($LTPoShURI)') | iex
+Update-LTService
+"@ -TimeOut 120000 -MaxLength 10240 -BatchSize $BatchSize -OfflineAction Skip -ResultPropertyName $RepairProperty -PassthroughObjects
                $ObjectCapture | Where-Object {$_.OperatingSystemName -like '*OS X*'}  | ForEach-Object {
                   If ($PSCmdlet.ShouldProcess("Automate Services on $($_.ComputerID) - $($_.ComputerName)",$Action)) {
                      Write-Host -BackgroundColor DarkGray -ForegroundColor Yellow "$($_.ComputerID) - $($_.ComputerName) - Attempting to $Action Automate Services - job will be submitted to online systems"
                      $_
                   }
-               } | Invoke-ControlCommand -Command '/usr/local/ltechagent/ltupdate' -TimeOut 60000 -MaxLength 10240 -BatchSize $BatchSize -OfflineAction Skip -ResultPropertyName $RepairProperty -PassthroughObjects
+               } | Invoke-ControlCommand -Command  @'
+[ -f /usr/local/ltechagent/ltupdate ]&&(
+/usr/local/ltechagent/ltupdate&&echo "Agent Update Completed Successfully"||echo "Agent Update failed or was not needed"
+)||echo "Error - Missing file /usr/local/ltechagent/ltupdate"
+'@.Replace("`r",'') -TimeOut 120000 -MaxLength 10240 -BatchSize $BatchSize -OfflineAction Skip -ResultPropertyName $RepairProperty -PassthroughObjects
                $ObjectCapture | Where-Object {$_.OperatingSystemName -like '*Linux*'}  | ForEach-Object {
                   If ($PSCmdlet.ShouldProcess("Automate Services on $($_.ComputerID) - $($_.ComputerName)",$Action)) {
                      Write-Host -BackgroundColor DarkGray -ForegroundColor Yellow "$($_.ComputerID) - $($_.ComputerName) - Attempting to $Action Automate Services - job will be submitted to online systems"
                      $_
                   }
-               } | Invoke-ControlCommand -Command '/usr/local/ltechagent/ltupdate' -TimeOut 60000 -MaxLength 10240 -BatchSize $BatchSize -OfflineAction Skip -ResultPropertyName $RepairProperty -PassthroughObjects
+               } | Invoke-ControlCommand -Command @'
+[ -f /usr/local/ltechagent/ltupdate ]&&(
+/usr/local/ltechagent/ltupdate&&echo "Agent Update Completed Successfully"||echo "Agent Update failed or was not needed"
+)||echo "Error - Missing file /usr/local/ltechagent/ltupdate"
+'@.Replace("`r",'') -TimeOut 120000 -MaxLength 10240 -BatchSize $BatchSize -OfflineAction Skip -ResultPropertyName $RepairProperty -PassthroughObjects
             )
             $ObjectCapture | Where-Object {!($_.OperatingSystemName -like '*Windows*' -or $_.OperatingSystemName -like '*OS X*' -or $_.OperatingSystemName -like '*Linux*')}  | ForEach-Object {
                Write-Host -BackgroundColor Yellow -ForegroundColor Red "$($_.ComputerID) - $($_.ComputerName) - $Action action for Operating System ($($_.OperatingSystemName)) is not supported at present in this module"
@@ -127,7 +146,22 @@ echo '}'
                      Write-Host -BackgroundColor DarkGray -ForegroundColor Yellow "$($_.ComputerID) - $($_.ComputerName) - Attempting to $Action Automate Services - job will be submitted to online systems"
                      $_
                   }
-               } | Invoke-ControlCommand -Powershell -Command "(new-object Net.WebClient).DownloadString('$($LTPoShURI)') | iex; Try {Restart-LTService} Catch {net stop ltsvcmon; net stop labvnc; net stop ltservice; TASKKILL /im ltsvcmon.exe /f; TASKKILL /im ltsvc.exe /f; TASKKILL /im lttray.exe /f; TASKKILL /im labvnc.exe /f; TASKKILL /im labtechupdate.exe /f /t; net start ltsvcmon; net start ltservice}" -TimeOut 120000 -MaxLength 10240 -BatchSize $BatchSize -OfflineAction Skip -ResultPropertyName $RepairProperty -PassthroughObjects
+               } | Invoke-ControlCommand -Powershell -Command @"
+(new-object Net.WebClient).DownloadString('$($LTPoShURI)') | iex
+Try { Restart-LTService }
+Catch {
+  net stop ltsvcmon
+  net stop labvnc
+  net stop ltservice
+  TASKKILL /im ltsvcmon.exe /f
+  TASKKILL /im ltsvc.exe /f
+  TASKKILL /im lttray.exe /f
+  TASKKILL /im labvnc.exe /f
+  TASKKILL /im labtechupdate.exe /f /t
+  net start ltsvcmon
+  net start ltservice
+}
+"@ -TimeOut 120000 -MaxLength 10240 -BatchSize $BatchSize -OfflineAction Skip -ResultPropertyName $RepairProperty -PassthroughObjects
                $ObjectCapture | Where-Object {$_.OperatingSystemName -like '*OS X*'}  | ForEach-Object {
                   If ($PSCmdlet.ShouldProcess("Automate Services on $($_.ComputerID) - $($_.ComputerName)",$Action)) {
                      Write-Host -BackgroundColor DarkGray -ForegroundColor Yellow "$($_.ComputerID) - $($_.ComputerName) - Attempting to $Action Automate Services - job will be submitted to online systems"
@@ -136,9 +170,14 @@ echo '}'
                } | Invoke-ControlCommand -Command @'
 LOGGEDUSERS=`who | grep console | awk '{ print $1 }'`
 echo "Stopping Services"
-(launchctl unload /Library/LaunchDaemons/com.labtechsoftware.LTSvc.plist; launchctl unload /Library/LaunchDaemons/com.labtechsoftware.LTUpdate.plist; for CURRUSER in $LOGGEDUSERS; do su -l $CURRUSER -c 'launchctl unload /Library/LaunchAgents/com.labtechsoftware.LTTray.plist'; done)
+(
+  launchctl unload /Library/LaunchDaemons/com.labtechsoftware.LTSvc.plist
+  launchctl unload /Library/LaunchDaemons/com.labtechsoftware.LTUpdate.plist
+  for CURRUSER in $LOGGEDUSERS; do su -l $CURRUSER -c 'launchctl unload /Library/LaunchAgents/com.labtechsoftware.LTTray.plist'; done
+)
 echo "Starting Services"
-sleep 5; launchctl load /Library/LaunchDaemons/com.labtechsoftware.LTSvc.plist
+sleep 5
+launchctl load /Library/LaunchDaemons/com.labtechsoftware.LTSvc.plist
 for CURRUSER in $LOGGEDUSERS; do su -l $CURRUSER -c 'launchctl load /Library/LaunchAgents/com.labtechsoftware.LTTray.plist'; done
 echo "Checking Services"
 (for CURRUSER in $LOGGEDUSERS; do su -l $CURRUSER -c 'launchctl list'; done) | grep -i "com.labtechsoftware"
@@ -159,7 +198,10 @@ launchctl list | grep -i "com.labtechsoftware"&&echo "LTService Restarted succes
                $ObjectCapture | Where-Object {$_.OperatingSystemName -like '*Windows*'} | ForEach-Object {
                   If ($PSCmdlet.ShouldProcess("Automate Services on $($_.ComputerID) - $($_.ComputerName)",$Action)) {
                      Write-Host -BackgroundColor DarkGray -ForegroundColor Yellow "$($_.ComputerID) - $($_.ComputerName) - Attempting to $Action Automate Services - job will be submitted to online systems"
-                     $_ | Invoke-ControlCommand -Powershell -Command "(new-object Net.WebClient).DownloadString('$($LTPoShURI)') | iex; Install-LTService -Server '$($Script:CWAServer)' -LocationID $($_.Location.Id) -InstallerToken '$($InstallerToken)' -Force -SkipDotNet" -TimeOut 300000 -MaxLength 10240 -BatchSize $BatchSize -OfflineAction Skip -ResultPropertyName $RepairProperty -PassthroughObjects
+                     $_ | Invoke-ControlCommand -Powershell -Command @"
+(new-object Net.WebClient).DownloadString('$($LTPoShURI)') | iex
+Install-LTService -Server '$($Script:CWAServer)' -LocationID $($_.Location.Id) -InstallerToken '$($InstallerToken)' -Force -SkipDotNet
+"@ -TimeOut 300000 -MaxLength 10240 -BatchSize $BatchSize -OfflineAction Skip -ResultPropertyName $RepairProperty -PassthroughObjects
                   }
                }
                $InstallerToken = Get-AutomateInstallerToken -InstallerType 5
@@ -171,15 +213,16 @@ LOCATIONID=$($_.Location.Id)
 cd /tmp&&(
  (rm -f cwaagent.zip; rm -Rf CWAutomate)&>/dev/null
  curl '$($Script:CWAServer)/LabTech/Deployment.aspx?InstallerToken=$($InstallerToken)' -s -o cwaagent.zip
- [[ `$(find cwaagent.zip -type f -size +1500000c 2>/dev/null) ]]&&(
-  echo SUCCESS-cwaagent.zip was downloaded
+ [[ `$(find cwaagent.zip -type f -size +700000c 2>/dev/null) ]]&&(
+  echo "SUCCESS-cwaagent.zip was downloaded"
   unzip -n -d CWAutomate cwaagent.zip &>/dev/null
   [ -f CWAutomate/config.sh ]&&(
-   [ -f /usr/local/ltechagent/uninstaller.sh ]&&(echo Existing installation found. Removing.; /usr/local/ltechagent/uninstaller.sh)
+   [ -f /usr/local/ltechagent/uninstaller.sh ]&&(echo "Existing installation found. Removing."; /usr/local/ltechagent/uninstaller.sh)
    cd /tmp/CWAutomate&&(
     mv -f config.sh config.sh.bak 2>/dev/null
     [ -f config.sh.bak ]&&sed "s/LOCATION_ID=[0-9]*/LOCATION_ID=`$LOCATIONID/" config.sh.bak > config.sh&&[ -f config.sh ]&&echo "SUCCESS-Installer Data Updated for location `$LOCATIONID" 
-    . ./config.sh ; installer -pkg ./LTSvc.mpkg -verbose -target /; [ -d /usr/local/ltechagent ]&&echo SUCCESS-Installer completed
+    . ./config.sh ; installer -pkg ./LTSvc.mpkg -verbose -target /
+    [ -d /usr/local/ltechagent ]&&echo "SUCCESS-Installer completed"
     launchctl list | grep -i "com.labtechsoftware"&&echo "LTService Started successfully"
    )  
   )||echo ERROR-Failed to extract
