10781133a924daebdd1e2fd68620b6f8cbe00335
redanthrax <redanthraxx@gmail.com>
2020-12-07 07:56:56 -0800

    Fix up tests, add .vscode to ignore

--- AutomateAPI.tests.ps1
+++ AutomateAPI.tests.ps1
@@ -7,14 +7,13 @@ Import-Module "$PSScriptRoot\\AutomateAPI.psm1" -Force
 $creds = Get-Content "$PSScriptRoot\\credentials.json" | Out-String | ConvertFrom-Json
 Describe AutomateAPI {
     It "AutomateAPI is valid PowerShell code" {
-        $psFile = Get-Content -Path "$PSScriptRoot\AutomateAPI.psm1" -ErrorAction Stop
+        $psFile = Get-Content -Path "$PSScriptRoot\\AutomateAPI.psm1" -ErrorAction Stop
         $errors = $null
         $null = [System.Management.Automation.PSParser]::Tokenize($psFile, [ref]$errors)
         $errors.Count | Should -Be 0
     }
 
     It "Connects to the Automate API" {
-        Write-Output $creds.Automate
         $password = ConvertTo-SecureString $creds.Automate.password -AsPlainText -Force
         $credentials = New-Object System.Management.Automation.PSCredential($creds.Automate.user, $password)
         $result = Connect-AutomateAPI -Server $creds.Automate.server -Credentials $credentials `
@@ -34,6 +33,6 @@ Describe AutomateAPI {
     }
     
     It "Control API returns a list of sessions" {
-        (Get-ControlSessions).Count | Should -BeGreaterThan 0
+        (Get-ControlSession).Count | Should -BeGreaterThan 0
     }
 }
